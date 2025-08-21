a7c35db6d116b93c6fa1992f97a4ceef64ab108f
HP <srinathsirimalla18@gmail.com>
2025-08-21 17:05:42 +0530

    First Commit RestAssured

---
+++ src/test/java/Day1/HTTPRequests.java
@@ -0,0 +1,20 @@
+package Day1;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+public class HTTPRequests {
+    @Test
+    public void getUsers(){
+        given()
+                .when()
+                    .get("https://reqres.in/api/users?page=2")
+                .then()
+                    .statusCode(200)
+                    .body("page",equalTo(2))
+                    //.body("email",equalTo("michael.lawson@reqres.in"))
+                    .log().all();
+    }
+}
