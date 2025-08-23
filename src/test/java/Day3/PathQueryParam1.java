b2d51e778fc38cb537654460ef2448f21abf3439
HP <srinathsirimalla18@gmail.com>
2025-08-23 15:57:48 +0530

    CRUD operations

---
+++ src/test/java/Day3/PathQueryParam1.java
@@ -0,0 +1,20 @@
+package Day3;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+public class PathQueryParam1 {
+    @Test
+    public void pathQueryParam(){
+        given()
+                .pathParam("mypath","users")
+                .queryParam("page",2)
+                .when()
+                .get("https://reqres.in/api/{mypath}")
+                .then()
+                .statusCode(200)
+                .log().all();
+    }
+}
